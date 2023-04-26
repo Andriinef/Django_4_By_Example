4349f373ae06d7ce4f0f1bff321442b1e55e0bd4
Andriinef <andrii.nefedov@nure.ua>
2023-04-26 18:40:46 +0300

    2_chapter

---
+++ 2_chapter/src/blog/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+# Создайте свои модели здесь.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DR", "Draft"  # Черновик
+        PUBLISHED = "PB", "Published"  # Публикация
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
